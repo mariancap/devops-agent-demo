81ff5eb32d7d8f215ff804ed5e3574e6a33eca13
mariancap <mareancap11@gmail.com>
2026-04-21 15:05:39 +0300

    chore: initial Spring Boot scaffold

---
+++ src/main/java/com/thesis/demo/DevopsAgentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thesis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAgentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAgentDemoApplication.class, args);
+	}
+
+}
